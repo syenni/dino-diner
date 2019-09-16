558e576a62daf189d15fa007ded41267a3622829
Seth Yenni <sethyenni@ksu.edu>
2019-09-15 19:17:00 -0500

    Still on last class

--- Menu/Entrees/DinoNuggets.cs
+++ Menu/Entrees/DinoNuggets.cs
@@ -6,6 +6,8 @@ namespace DinoDiner.Menu.Entrees
 {
     public class DinoNuggets
     {
+        private int numOfNuggets = 6;
+
         public double Price { get; set; }
         public uint Calories { get; set; }
         public List<string> Ingredients
@@ -13,13 +15,23 @@ namespace DinoDiner.Menu.Entrees
             get
             {
                 List<string> ingredients = new List<string>();
+                for (int i = 0; i < numOfNuggets; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
             }
         }
 
         public DinoNuggets()
         {
             this.Price = 4.25;
-            this.Calories = 59 * 6;
+            this.Calories = 59 * Convert.ToUInt32(numOfNuggets);
+        }
+
+        public void AddNugget()
+        {
+            numOfNuggets++;
         }
     }
 }
