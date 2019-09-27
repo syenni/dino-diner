9b5082723bf77465543b54474345f873528a8e64
Seth Yenni <sethyenni@users.campus>
2019-09-27 14:49:11 -0500

    In class work, Friday

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public abstract class Drink
+    {
+
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+
+        public abstract Size Size { get; set; }
+
+        public List<string> Ingredients { get; protected set; }
+
+    }
+}
---
+++ MenuTest/ComboTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu;
+
+
+namespace MenuTest
+{
+    public class ComboTest
+    {
+        [Fact]
+        public ComboMustBeSuppliedAnEntree()
+        {
+            CretaciousCombo combo = new CretaciousCombo();
+        }
+    }
+}
